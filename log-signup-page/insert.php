e97e710afb599cc76d9c7dc7967f280848a4639f
Anjli22 <kushwahanjali22@gmail.com>
2021-10-21 19:04:10 +0530

    d

--- log-signup-page/insert.php
+++ log-signup-page/insert.php
@@ -1,17 +1,17 @@
 <?php
-// include('connection.php');
+ include('connection.php');
 
     $uname =$_POST['uname'];
     $email1=$_POST['email1'];
     $password1=$_POST['password1'];
-    // $cpass=$_POST['cpass'];
+   
     echo "hello";
-    // $query = $db->prepare('INSERT INTO student(uname,email,pass) VALUES(?,?,?)');
-    // $data=array($uname,$email1,$password1);
-    // $query->execute($data);
-    //  if($excute)
-    //  echo 0;
-    //  else
+    $query = $db->prepare('INSERT INTO student(uname,email,pass) VALUES (?,?,?)');
+    $data=array($uname,$email1,$password1);
+    $execute=$query->execute($data);
+     if($execute)
+     echo 0;
+     else
     echo 1;
 
 ?>
