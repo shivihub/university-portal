889717221cf309ba2c47577df3c73287480d51cc
shivihub <yadavshivanisingh@gmail.com>
2021-12-02 15:39:13 +0530

    pushing chnges

--- ajax/connection.php
+++ ajax/connection.php
@@ -2,8 +2,7 @@
 	$servername="localhost";
 	$username="root";
 	$password="";
-
 	$db = new PDO("mysql:host=$servername;dbname=uni",$username,$password);
-
 	$db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+
     ?>
